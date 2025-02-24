cb8e07497133793c763e0ab3dbf67958e1dbc2ce
Mümin Kayacı <kayacimumin@gmail.com>
2025-02-24 15:59:07 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,9 @@
 
-document.querySelector('.menu-toggle').addEventListener('click', () => {
-    document.querySelector('.nav-menu').classList.toggle('active');
-});
-
-
 document.querySelectorAll('.navbar a').forEach(anchor => {
     anchor.addEventListener('click', function(e) {
         e.preventDefault();
         const section = document.querySelector(this.getAttribute('href'));
         section.scrollIntoView({ behavior: 'smooth' });
-        document.querySelector('.nav-menu').classList.remove('active'); // Menüyü kapat
     });
 });
 
@@ -27,8 +21,8 @@ window.onload = () => {
     }, 500);
 };
 
-
 window.addEventListener('scroll', () => {
+ 
     document.querySelectorAll('.small-heart').forEach(heart => {
         const heartPos = heart.getBoundingClientRect().top;
         const windowHeight = window.innerHeight;
@@ -40,27 +34,13 @@ window.addEventListener('scroll', () => {
 
     const video = document.getElementById('autoPlayVideo');
     const videoPos = video.getBoundingClientRect().top;
-    if (videoPos < windowHeight && videoPos >= 0) {
-        if (!video.playing) {
+    const windowHeight = window.innerHeight;
+
+    if (videoPos < windowHeight && videoPos >= 0) { 
+        if (!video.playing) { 
             video.play();
         }
-    } else {
+    } else { 
         video.pause();
-    }
-
-    const audio = document.getElementById('autoPlayAudio');
-    const audioPos = audio.getBoundingClientRect().top;
-    if (audioPos < windowHeight && audioPos >= 0) {
-        if (!audio.playing) {
-            audio.volume = 0.5;
-            audio.play().catch(error => {
-                console.log("Otomatik oynatma engellendi:", error);
-                document.body.addEventListener('click', () => {
-                    audio.play();
-                }, { once: true });
-            });
-        }
-    } else {
-        audio.pause();
     }
 });
